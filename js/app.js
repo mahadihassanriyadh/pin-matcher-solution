bb9ecb785e80dec3f14b08fcb3aca16a7ca2a79d
Mahadi Hassan Riyadh <mahadi_hassan3931@yahoo.com>
2021-08-19 23:44:45 +0600

    used event bubble for shortcut

--- js/app.js
+++ js/app.js
@@ -28,3 +28,23 @@ function generatePin(){
     const pin = getPin();
     document.getElementById('display-pin').value = pin;
 }
+
+
+
+document.getElementById('key-pad').addEventListener('click', function(event){
+    const number = event.target.innerText;
+    const calcInput = document.getElementById('typed-numbers');
+    if (isNaN(number)){
+        if(number == 'C'){
+            calcInput.value = '';
+        }
+        else if(number == '<'){
+            console.log(previousNum);
+        }
+    }
+    else {
+        const previousNum = calcInput.value;
+        const newCalc = previousNum + number;
+        calcInput.value = newCalc;
+    }
+})
